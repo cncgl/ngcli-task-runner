f553b3cb8a3eb752e56eb50966ea3c422b1c0624
cncgl <concigel@gmail.com>
2016-03-24 12:53:57 +0900

    fix test task

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Broc = require("./broc.js");
 var broc = new Broc();
-var karma = require("karma").server;
+var karma = require("karma").Server;
 var path = require("path");
 
 if(process.argv.indexOf('--build') > -1){
@@ -10,11 +10,12 @@ if(process.argv.indexOf('--serve') > -1){
   broc.run('serve');
 }
 if(process.argv.indexOf('--test') > -1){
-  karmaFile = path.join(__dirname+'../../../','karma.conf.js');
+  karmaFile = path.join(__dirname+'/../../','karma.conf.js');
   singleRun = false;
   config = {
     configFile: karmaFile,
     singleRun: singleRun
   };
-  karma.start(config);
+  var server = new Server(config);
+  server.start();
 }
